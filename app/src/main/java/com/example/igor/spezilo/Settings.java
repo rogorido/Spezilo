886031ca758932d40a436186da55d9c244e2e346
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2016-09-12 17:10:56 +0200

    Adding code for settings activity

--- app/src/main/java/com/example/igor/spezilo/Settings.java
+++ app/src/main/java/com/example/igor/spezilo/Settings.java
@@ -2,12 +2,41 @@ package com.example.igor.spezilo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
+
+    PurchaseSQLiteHelper dbh;
+    SQLiteDatabase db;
+    Button btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        btnDelete = (Button) findViewById(R.id.btnDeleteDB);
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                deleteDB();
+            }
+        });
     }
+
+    private void deleteDB() {
+        db = dbh.getWritableDatabase();
+
+        if (db!=null) {
+
+            String sqlSentence = "DELETE FROM purchases";
+            db.execSQL(sqlSentence);
+
+        }
+
+        db.close();
+    }
+
 }
