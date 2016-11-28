a465e9ddd3aa5a3e93ac22432b3446ee64f2d151
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2016-11-28 07:34:15 +0100

    Nueva clase PurchaseAdapter

---
+++ app/src/main/java/com/example/igor/spezilo/PurchasesAdapter.java
@@ -0,0 +1,49 @@
+package com.example.igor.spezilo;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.CursorAdapter;
+
+
+public class PurchasesAdapter extends CursorAdapter {
+    public PurchasesAdapter(Context context, Cursor cursor) {
+        super(context, cursor, 0);
+    }
+
+    /*
+    uso el layout de categories_item por no hacer otro...
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.purchase_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find fields to populate in inflated template
+        TextView tvDate = (TextView) view.findViewById(R.id.tvDatePurchase);
+        TextView tvAmount = (TextView) view.findViewById(R.id.tvAmountPurchase);
+        TextView tvCategory = (TextView) view.findViewById(R.id.tvCategoryPurchase);
+        TextView tvPerson = (TextView) view.findViewById(R.id.tvPersonPurchase);
+        TextView tvPlace = (TextView) view.findViewById(R.id.tvPlacePurchase);
+
+        // Extract properties from cursor
+        String date = cursor.getString((cursor.getColumnIndex("date")));
+        double amount = cursor.getDouble(cursor.getColumnIndex("TOTAL"));
+        String category = cursor.getString(cursor.getColumnIndex("category"));
+        String person = cursor.getString(cursor.getColumnIndex("person"));
+        String place = cursor.getString(cursor.getColumnIndex("place"));
+
+        // Populate fields with extracted properties
+        tvDate.setText(date);
+        tvAmount.setText(String.valueOf(amount));
+        tvCategory.setText(category);
+        tvPerson.setText(person);
+        tvPlace.setText(place);
+
+    }
+}
